f570401c934218fd28e7dcaa1d39b617036db9af
haond10adp <39975444+haond10adp@users.noreply.github.com>
2020-03-23 18:18:56 +0700

    fix bug: auto display image when load

--- script.js
+++ script.js
@@ -15,4 +15,4 @@ function windowOnClick(event) {
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+document.addEventListener("click", windowOnClick);
